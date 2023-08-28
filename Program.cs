b77571e6c1d45773de472c156387967a74d2e41e
ThanawatC <thanawat.chu.au@gmail.com>
2023-08-28 11:45:59 +1000

    Create Program.cs
    New code line

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AT1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNum;
+            myNum = 15;
+            Console.WriteLine(myNum);
+
+        }
+    }
+}
